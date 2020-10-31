c62c651d1f15e0e8b9d3188797bb8924b4fe8f19
Sheepolution <danielhaazen@hotmail.com>
2020-10-31 09:06:56 +0100

    Remove unused code

--- code/src/Index.ts
+++ code/src/Index.ts
@@ -3,15 +3,11 @@ require('dotenv').config();
 import BotManager from './Managers/BotManager';
 import Discord from './Providers/Discord';
 import './Utils/MonkeyPatches';
-import './Providers/SQL';
 
 class Main {
 
     constructor() {
         Discord.SetEventReadyCallback(BotManager.OnReady);
-        // Discord.SetEventMessageCallback(BotManager.OnMessage);
-        // Discord.SetEventReactionAddCallback(BotManager.OnReaction);
-        // Discord.SetEventReactionRemoveCallback(BotManager.OnReaction);
         Discord.Init();
     }
 }
