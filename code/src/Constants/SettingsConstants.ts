9dde24d1bd747a6208d4f9ca63a7e0c38f0783be
Sheepolution <danielhaazen@hotmail.com>
2021-01-05 12:59:45 +0100

    Remove unused BOT_ID constant

--- code/src/Constants/SettingsConstants.ts
+++ code/src/Constants/SettingsConstants.ts
@@ -1,5 +1,4 @@
 export default class SettingsConstants {
-    public static readonly BOT_ID = process.env.BOT_ID || '';
     public static readonly CHANNEL_NEWS_ID = process.env.CHANNEL_NEWS_ID || '';
     public static readonly CHANNEL_SPORTS_ID = process.env.CHANNEL_SPORTS_ID || '';
 }
